08b385397163b70968211a9b760e424bb4d3fc8e
Asden <edthevargas@gmail.com>
2025-02-03 14:54:14 -0500

    init header

---
+++ frontend/src/components/taskManager/header.tsx
@@ -0,0 +1,18 @@
+const taskManagerPageHeader = () =>{
+    /*
+    header
+        tabs 
+        search
+        add new button (blue)
+    */
+    return(
+        <div className="header">
+            <div className="header__logo">LOGO</div>
+            <div className="header__tabs">
+            </div>
+            <div className="header__search"></div>
+            <div className="header__addTaskBtn"></div>
+        </div>
+    )
+}
+export default taskManagerPageHeader
