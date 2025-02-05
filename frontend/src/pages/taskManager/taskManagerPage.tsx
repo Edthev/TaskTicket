5f3ce6887d9c5be16f7d799e0279abfee3fd78dc
Asden <edthevargas@gmail.com>
2025-02-05 16:30:37 -0500

    import and add header

--- frontend/src/pages/taskManager/taskManagerPage.tsx
+++ frontend/src/pages/taskManager/taskManagerPage.tsx
@@ -1,15 +1,15 @@
+import Header from "../../components/taskManager/header"
 const taskManagerPage = () =>{
     /*
     header
         tabs 
         search
         add new button (blue)
-        
-
-
     */
     return(
-        <></>
+        <>
+         <Header/>
+        </>
     )
 }
 export default taskManagerPage
