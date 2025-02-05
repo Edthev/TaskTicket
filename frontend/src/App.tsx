5c3e239e4e27b89c2741a39eb1c7ee8e75d7c2ab
Asden <edthevargas@gmail.com>
2025-02-05 16:31:51 -0500

    add routes and TaskManagerPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.scss'
+import TaskManager from "./pages/taskManager/taskManagerPage"
 
 function App() {
 
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+      <Route path="/" element={<TaskManager/>}></Route>
+      </Routes>
+    </Router>
   )
 }
 
