bb3b751c07454ddb5896c9db8d03badd3cfb7a07
Asden <edthevargas@gmail.com>
2025-02-06 11:42:02 -0500

    add header styling minor tweaks pending

--- frontend/src/components/taskManager/header.tsx
+++ frontend/src/components/taskManager/header.tsx
@@ -1,4 +1,6 @@
 import "./header.scss"
+import Logo from "../../assets/Logo.svg"
+import SearchIcon from "../../assets/searchIcon.svg"
 const taskManagerPageHeader = () =>{
     /*
     header
@@ -8,10 +10,18 @@ const taskManagerPageHeader = () =>{
     */
     return(
         <div className="header">
-            <div className="header__logo">LOGO</div>
+            {/* TODO turn logo into route to home page */}
+            <div className="header__logo"><img src={Logo} className="logo"/> </div>
+            {/* TODO make title dynamic based on route */}
             <h1 className="header__title">Dashboard</h1>
-            <div className="header__addTaskBtn"></div>
-            <button className="header__search">+ add new</button>
+            <button className="header__addTaskBtn">Add New +</button>
+            {/* FIXME visual glitch with search icon */}
+            <div className="header__search">
+                {/* TODO make search icon a enter button */}
+                <img src={SearchIcon} className="searchIcon"/>
+                {/* TODO search table with fuzzy search */}
+                <input placeholder="Search" className="searchInput"/>
+            </div>
             <div className="header__user"></div>
         </div>
     )
