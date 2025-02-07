5d19f5e93ca5ece4c653c9917fafd2a2f20a2d1e
Asden <edthevargas@gmail.com>
2025-02-07 10:25:13 -0500

    modal is functional and styled; needs styling upgrade

--- frontend/src/components/taskManager/modal.tsx
+++ frontend/src/components/taskManager/modal.tsx
@@ -1,26 +1,28 @@
 import "./modal.scss"
-const modal=()=>{
+const modal=({isModalOpen, setIsModalOpen}: any)=>{
+    console.log("test:",isModalOpen)
+    if(isModalOpen){
     return(
-        <div className="modal">
-            <div className="modal__close"></div>
+        <div className="modal" id="modal">
+            <button className="modal__close" onClick={()=>{setIsModalOpen(false)}}>X</button>
             <div className="modal__input">
                 <form>
                     <label>Address:</label>
                     <input type="text" name="address" placeholder="Address" required /> 
-                    <label></label>
-                    <input type="text" name="apt" placeholder="apt" required/>
-                    <label></label>
-                    <input type="text" name="number" placeholder="number" required/>
-                    <label></label>
-                    <input type="text" name="notes" placeholder="notes" required/>
-                    <label></label>
-                    <input type="text" name="status" placeholder="status" required/>
+                    <label>Apartment:</label>
+                    <input type="text" name="apt" placeholder="Apt" required/>
+                    <label>Phone Number:</label>
+                    <input type="text" name="number" placeholder="Number" required/>
+                    <label>Notes:</label>
+                    <input type="text" name="notes" placeholder="Notes" required/>
+                    <label>Status:</label>
+                    <input type="text" name="status" placeholder="Status" required/>
+            <div className="modal__submit">
+                <button onSubmit={()=>{setIsModalOpen(false)}}>Submit</button>
+            </div>
                 </form>
             </div>
-            <div className="modal__submit">
-                <button>Submit</button>
-            </div>
         </div>
-    )
+    )}
 }
 export default modal
