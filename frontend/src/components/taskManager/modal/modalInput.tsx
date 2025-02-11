f037ddbb7126bcd891eebe4fc416e0e692b541be
Asden <edthevargas@gmail.com>
2025-02-11 16:17:22 -0500

    init modal components

---
+++ frontend/src/components/taskManager/modal/modalInput.tsx
@@ -0,0 +1,11 @@
+import SearchIcon from "../../../assets/searchIcon.svg"
+const ModalInput = ({placeholder,icon, state, error}:any)=>{
+    return (
+        <div className="modal__input">
+            <label>{placeholder}</label>
+            <div>
+                    
+            </div>
+        </div>
+    )
+}
