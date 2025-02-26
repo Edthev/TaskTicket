5cd9df3dab5aa640904b0d7548e4676695556839
Asden <edthevargas@gmail.com>
2025-02-26 13:57:29 -0500

    stable submit form styling

--- frontend/src/components/taskManager/modal/modalForm.tsx
+++ frontend/src/components/taskManager/modal/modalForm.tsx
@@ -1,28 +1,34 @@
-import "./modal.scss"
+import "./modalForm.scss"
+import Input from "./modalInput"
+import Dropdown from "./modalDropdown"
+import addressIcon from "../../../assets/buildingsIcon.svg"
+import aptIcon from "../../../assets/doorIcon.svg"
+import phoneIcon from "../../../assets/phoneIcon.svg"
+import noteIcon from "../../../assets/noteIcon.svg"
+
 const modal=({isModalOpen, setIsModalOpen}: any)=>{
     console.log("test:",isModalOpen)
     if(isModalOpen){
     return(
-        <div className="modal" id="modal">
-            <button className="modal__close" onClick={()=>{setIsModalOpen(false)}}>X</button>
-            <div className="modal__input">
-                <form>
-                    <label>Address:</label>
-                    <input type="text" name="address" placeholder="Address" required /> 
-                    <label>Apartment:</label>
-                    <input type="text" name="apt" placeholder="Apt" required/>
-                    <label>Phone Number:</label>
-                    <input type="text" name="number" placeholder="Number" required/>
-                    <label>Notes:</label>
-                    <input type="text" name="notes" placeholder="Notes" required/>
-                    <label>Status:</label>
-                    <input type="text" name="status" placeholder="Status" required/>
-            <div className="modal__submit">
-                <button onSubmit={()=>{setIsModalOpen(false)}}>Submit</button>
-            </div>
+        <>
+            <div className="background"/>
+            <div className="modal">
+                <div className="modal__header">
+                    <span className="title">Add Task</span>
+                    <button className="close" onClick={()=>{setIsModalOpen(false)}}>X</button>
+                </div>
+                <form className="modal__form">
+                    <Input placeholder="Address" icon={addressIcon} state={true} error={false}/>
+                    <Input placeholder="Apartment" icon={aptIcon} state={true} error={false}/>
+                    <Input placeholder="Phone Number" icon={phoneIcon} state={true} error={false}/>
+                    {/* <Dropdown placeholder="Status" state={true} error={false}/> */}
+                    <Input placeholder="Notes" icon={noteIcon} state={true} error={false}/>
+                    <div className="submit">
+                        <button onSubmit={()=>{setIsModalOpen(false)}}>Submit</button>
+                    </div>
                 </form>
             </div>
-        </div>
+        </>
     )}
 }
 export default modal
