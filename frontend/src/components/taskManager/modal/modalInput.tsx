910b571ec7398ef6f4a25251a653226395295a92
Asden <edthevargas@gmail.com>
2025-03-20 16:25:43 -0400

    add displayError

--- frontend/src/components/taskManager/modal/modalInput.tsx
+++ frontend/src/components/taskManager/modal/modalInput.tsx
@@ -1,12 +1,20 @@
-import SearchIcon from "../../../assets/searchIcon.svg"
+// import SearchIcon from "../../../assets/searchIcon.svg"
 const ModalInput = ({placeholder,icon, state, error}:any)=>{
-    return (
-        <div className="modal__input">
-            <label>{placeholder}</label>
-            <img src={SearchIcon}/>
-            <div>
-                    
+    const displayError =()=>{
+        return(
+            <div className="error">
+                You can't leave this field empty
             </div>
+        )
+    }
+    return (
+        <div className="input">
+            <label className="label">{placeholder}</label>
+            <div>
+                <img className="icon" src={icon}/>
+                <input placeholder={placeholder}/>
+            </div>
+            {error ? displayError() : <></>}
         </div>
     )
 }
