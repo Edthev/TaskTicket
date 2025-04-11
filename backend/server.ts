939ed79ea3cdb23c6737d62496248b479e3011a1
Asden <edthevargas@gmail.com>
2025-04-11 10:57:10 -0400

    init backend

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,16 @@
-// 
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.get('/', (_req, res) => {
+  res.send({'Test':"init"});
+});
+
+app.listen(PORT, () => {
+    console.log(process.env.PORT)
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
