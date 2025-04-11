3401a9d5dccf326cee53a3bfd0b54d330f09921a
Asden <edthevargas@gmail.com>
2025-04-11 15:11:18 -0400

    make code more readable

--- backend/server.ts
+++ backend/server.ts
@@ -6,19 +6,25 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST'],
-}))
+// middleware
+// app.use(cors())
 app.use(express.json()); 
 
-app.get('/', (_req, res) => {
-  res.send({'Test':"init"});
+// show backend is working from terminal
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
+// show backend is working from frontend
+app.get('/', (_req, res) => {
+  res.send({'Server':"Working"});
+});
+
+// handle posts to backend
 app.post('/submit', (req, res) => {
   const data = req.body;
   console.log('Received "/submit":', data);
+  // handle empty data
   if(data){
     console.log("status:201")
       res.status(201).json({
@@ -34,6 +40,3 @@ app.post('/submit', (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
