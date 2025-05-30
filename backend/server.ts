bdba87419437db9b02316cc7ce3d2737ff919ef5
Asden <edthevargas@gmail.com>
2025-05-30 16:20:11 -0400

    add middleware and give response to frontend when not connected

--- backend/server.ts
+++ backend/server.ts
@@ -1,13 +1,14 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import { db } from './db';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // middleware
-// app.use(cors())
+app.use(cors())
 app.use(express.json()); 
 
 // show backend is working from terminal
@@ -16,9 +17,17 @@ app.listen(PORT, () => {
 });
 
 // show backend is working from frontend
-app.get('/', (_req, res) => {
-  res.send({'Server':"Working"});
+app.get('/', async (_req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const [rows] = await db.query('SELECT NOW() AS currentTime');
+    const currentTime = Array.isArray(rows) && rows.length > 0 ? (rows as any)[0].currentTime : null;
+    res.json({ DB_Time: currentTime });
+  } catch (err) {
+    console.error('❌ Query error:', err);
+    res.status(500).json({ Server: 'DB error' });
+  }
 });
+
 
 // handle posts to backend
 app.post('/submit', (req, res) => {
