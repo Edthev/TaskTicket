f85a54d969a27d6c78c1ad93ff335dc0d24bfb15
Edward Vargas <edthevargas@gmail.com>
2025-07-13 16:19:06 -0400

    support shared .env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const envDir = path.resolve(__dirname, '..');
+  
+  const env = loadEnv(mode, envDir, '');
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.FRONTEND_PORT),
+    },
+  };
 })
