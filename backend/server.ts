671717a3ef451b64a11882d5243450533ec3734c
Edward Vargas <edthevargas@gmail.com>
2025-07-13 21:33:05 -0400

    init mangoDB

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,82 @@
+import dotenv from 'dotenv';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
-import { db } from './db';
+require('dotenv').config({ path: '../.env' });
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
+const URI = process.env.MANGO_DB
+const DB = process.env.DB_NAME
+
+if(!PORT || !URI || !DB){
+  //TODO handle error on frontend
+  console.error("Error with ENV")
+}
+
+// handle Errors
+const handleErrors=async(err:string,errObj:object)=>{
+}
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = await client.db(DB) 
+    const collections = await db.listCollections().toArray();
+
+    /*TODO
+    take length of collections array
+    any field that starts with "_" is considered a hidden element
+    arrays are used like notions multi-select
+    dont handle objects (for now)
+    */
+    if(!collections.length){
+      //TODO handle error on frontend
+      console.log("Error with DB")
+    }else{ 
+      // Display collection names
+      console.log("Collections in the database:");
+      for (const col of collections) {
+        const doc = await db.collection(col.name).findOne({});
+        if (doc) {
+          const fields = Object.keys(doc);
+          // remove fields that start with _
+          // remove fields that have a type of obj
+          console.log(`Fields in collection '${col.name}'`);
+          for (const [key, value] of Object.entries(doc)) {
+            if(key[0]=="_"){
+            }else{
+              console.log(`- ${key}: ${typeof value}`);
+            }
+          }
+
+        } else {
+          console.log(`Collection '${col.name}' is empty.`);
+        }
+      };
+
+      // const collection = await client.db(DB).collection("users").find({}).toArray();
+    }
+  }catch(e){
+    //TODO handle error on frontend
+    console.error("Error:",e)
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run()
 
 // middleware
 app.use(cors())
@@ -19,12 +90,7 @@ app.listen(PORT, () => {
 // show backend is working from frontend
 app.get('/', async (_req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
-    const [rows] = await db.query('SELECT NOW() AS currentTime');
-    const currentTime = Array.isArray(rows) && rows.length > 0 ? (rows as any)[0].currentTime : null;
-    res.json({ DB_Time: currentTime });
   } catch (err) {
-    console.error('❌ Query error:', err);
-    res.status(500).json({ Server: 'DB error' });
   }
 });
 
