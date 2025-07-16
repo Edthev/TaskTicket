baf5d0039863c3ae6935798577b0c91c74dd426f
Edward Vargas <edthevargas@gmail.com>
2025-07-16 19:34:27 -0400

    update config to fix env error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const envDir = path.resolve(__dirname, '..');
-  
-  const env = loadEnv(mode, envDir, '');
+  const envDir = path.resolve(__dirname, '..'); // one level up
+  const env = loadEnv(mode, envDir, ''); // '' to load all vars
+
   return {
     plugins: [react()],
+    envDir, // let Vite know about your custom .env directory
     server: {
-      port: parseInt(env.FRONTEND_PORT),
+      port: parseInt(env.FRONTEND_PORT), // this works at build time
+    },
+    define: {
+      'process.env': env, // optional if you use process.env somewhere
     },
   };
-})
+});
